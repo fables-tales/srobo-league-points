74bbf72742a6f3677b8586108778cdc87a5e84ac
Sam Phippen <samphippen@googlemail.com>
2013-04-07 17:07:25 +0100

    Add a spec for two two way ties
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -64,6 +64,23 @@ describe ComputesLeagueScores do
       }
     end
   end
+
+  context "two two way ties" do
+    it "gives 3.5 to the top two teams, and 1.5 to the bottom two" do
+      l = ComputesLeagueScores.new
+      l.add_team("ABC", 11)
+      l.add_team("FAC", 11)
+      l.add_team("CWD", 7)
+      l.add_team("TRD", 7)
+      expect(l.league_scores).to be == {
+        "ABC" => 3.5,
+        "FAC" => 3.5,
+        "CWD" => 1.5,
+        "TRD" => 1.5
+      }
+    end
+  end
+
   context "with a three way tie at the bottom" do
     it "gives 4 to the top team, and 2 to the bottom 3" do
       l = ComputesLeagueScores.new
